be81fe7bea8acd7c9b5531c612cd0859f0bb55de
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2022-11-10 13:38:15 +0100

    Initial commit

---
+++ src/Paddle.java
@@ -0,0 +1,17 @@
+public class Paddle {
+private int x;
+private int y;
+private int width;
+
+public Paddle() {
+    x = 10;
+    y = 10;
+    width =10;
+}
+public Paddle(int x, int y, int w){
+    this.x = x;
+    this.y = y;
+    this.width = w;
+}
+
+}
